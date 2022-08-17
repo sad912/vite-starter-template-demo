f1590b9ebc7bec6d2f84736e9f6cbe9ff048021e
sad.wang <sad912@foxmail.com>
2022-08-17 22:15:15 +0800

    chore:import unocss

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import './style.css'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
+import 'uno.css'
 
 createApp(App).use(router, store).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsxPlugin from "@vitejs/plugin-vue-jsx";
+import Unocss from "unocss/vite"
+import {presetAttributify, presetUno} from 'unocss'
 
 export default defineConfig({
     plugins: [
         vue(),
-        vueJsxPlugin()
+        vueJsxPlugin(),
+        Unocss({
+            presets: [
+                presetAttributify({ /* preset options */}),
+                presetUno(),
+                // ...custom presets
+            ],
+            rules: [
+                ['m-1', { margin: '0.25rem' }],
+            ]
+        })
     ]
 })
